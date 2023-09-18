203e92d8e3bd339e33e51e117cd9edea08d568cc
Ordinarius Metallum <Metaruhebi@gmail.com>
2023-09-18 14:32:25 +0300

    Test of lesson No. 79
    pages 508, Test of lesson No. 79, task 1.

--- Ravesli_code/Ravesli_code.cpp
+++ Ravesli_code/Ravesli_code.cpp
@@ -1,23 +1,17 @@
-﻿#include <iostream>
+﻿//Test of lesson No. 79, Task 1.
+//Display the following array using a loop: int array[] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
+
+#include <iostream>
 
 using std::cout;
 using std::cin;
 
-namespace Animals 
-{
-	enum Animals 
-	{
-		CHICKEN,
-		LION,
-		GIRAFFE,
-		ELEPHANT,
-		DUCK,
-		SNAKE,
-		MAX_ANIMALS
-	};
-}
-
 int main() {
-	int array[Animals::MAX_ANIMALS] = { 2, 4, 4, 4, 2, 0 };
-	cout << "The number of legs of an elephant " << array[Animals::ELEPHANT];
+	int array[] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
+	const int allArray = sizeof(array) / sizeof(array[0]);
+	int number = 0;
+	for (int index = 0; index < allArray; ++index) {
+		number = array[index];
+		cout << number << " ";
+	}
 }
